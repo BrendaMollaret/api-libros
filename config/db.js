0658df71cefa51e6274b855c1e8368be507e4cb1
Brenda Mollaret <mollaretbrenda@gmail.com>
2025-10-03 21:16:16 -0300

    conexión a vercel

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const dbconnect = async () => {
-    try {
-        await mongoose.connect("mongodb://localhost:27017/dbGestorBiblioteca");
-        console.log('Conexión a la base de datos fue exitosa');
-    } catch (error) {
-        console.error('Error en la conexión a la base de datos', error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Conexión exitosa a MongoDB Atlas");
+  } catch (error) {
+    console.error("Error al conectar a MongoDB Atlas:", error.message);
+  }
 };
 
 module.exports = dbconnect;
